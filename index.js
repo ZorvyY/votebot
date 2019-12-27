687805999d39625cd38d38704ea3ee65ae116534
Zoravur Singh <zoravur.s@gmail.com>
2019-12-27 18:21:20 -0500

    prep for local exec

--- index.js
+++ index.js
@@ -1,100 +1,36 @@
 const bot = require('./bot.js');
 const fs = require('fs')
+const argv = require('yargs').argv
 
-async function getBrowser(proxy) {
-  if (process.env.LAMBDA) {
-    const chromium = require('chrome-aws-lambda');
-    return chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-    });
-  } else {
-    require('console-stamp')(console, 'HH:MM:ss');
-    const puppeteer = require('puppeteer');
+const execPath = argv.chromepath //|| '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
-    if (proxy) {
-      return puppeteer.launch({
-        args: [
-          `--proxy-server=${proxy}`,
-        ]
-      });
-    } else return puppeteer.launch();
-  }
-}
-
-
-// local only code
-function getProxies() {
-  const proxies = fs.readFileSync('savedProxies.txt', { encoding: 'utf8' }).split('\n')
-  return proxies;
-}
-
-const saveProxy = (() => {
-  let proxies = []
-  return (proxy => {
-    proxies.push(proxy);
-    if (proxies.length == 1) {
-      fs.appendFileSync('savedProxies.txt', proxies.join('\n') + '\n')
-      proxies.length = 0;
-    }
+async function getBrowser() {
+  const puppeteer = require('puppeteer-core')
+  return puppeteer.launch({
+    executablePath: execPath
   })
-})()
+}
 
+const format = result => {
+  return `[${new Date()}] ${result.value ? `Value: ${result.value}` : `Error: ${result.error}`}`
+}
 
 async function main() {
-  const pLimit = require('p-limit');
- 
-  const limit = pLimit(9);
-
-  let votes = /* getProxies() */[].concat(null).map(proxy => limit(async proxy => {
-    let browser = await getBrowser(proxy);
-    let result = {
-      proxy: proxy,
-      value: null,
-      error: null,
-    };
-    try {
-      result.value = await bot.vote(browser);
-    }
-    catch (err) {
-      result.error = String(err)
-    } finally {
-      await browser.close();
-      console.log(result);
-      //if (result.error == null) saveProxy(proxy)
-      return result;
-    }
-  }, proxy) );
-  
-  //console.log(votes)
-  //console.log(getProxies())
-  //console.log(await Promise.all(votes))
-
-}
-main()
-
-/*
-exports.handler =  async function(event, context) {
-
-
-
   let browser = await getBrowser();
+  let result = {
+    value: null,
+    error: null,
+  };
   try {
-    return await bot.vote(browser);
+    result.value = await bot.vote(browser);
   }
-  catch (error) {
-    console.log(error);
+  catch (err) {
+    result.error = String(err)
   } finally {
     await browser.close();
+    fs.appendFileSync('log.txt', format(result));
+    return result;
   }
-
 }
-
-if (!process.env.LAMBDA) {
-  exports.handler({}, {})
-}
-*/
-
+main()
 
